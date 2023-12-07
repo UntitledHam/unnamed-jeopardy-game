15a61459b8bca07e9df8993da34618ce92bc87da
untitledham <namelessham@gmail.com>
2023-12-07 01:40:08 -0500

    Moved style to a separate file.

--- app.py
+++ app.py
@@ -12,56 +12,8 @@ category_names = ["food_and_drink", "music", "geography", "random", "random"]
 game.generate_categories(category_names)
 
 
-style = """ 
-
-body {
-    background-color: #301e83;
-    font-family: "Times New Roman", Times, serif;
-}
-a {
-    color: yellow;
-    font-size: min(180%);
-}
-.leaderboard {
-    width: 20%;
-    height: 50%;
-    color: white;
-    position: absolute;
-    background-color: #37287f;
-    border: 4px white solid;
-    h1 {
-        text-align: center;
-        font-size: 340%;
-    }
-    p {
-        font-size: 150%;
-        color: yellow;
-    }
-    
-}
-
-div {
-    width: 65%;
-    height: 65%;
-    padding: 20px;
-}
-.container {
-  margin-left: 45%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-gap: 5px;
-}
-.container div {
-  background-color: blue;
-  aspect-ratio: 1;
-  text-align: center;
-  color: white;
-  font-family: "Times New Roman", Times, serif;
-  font-size: min(150%);
-  border: 1px white solid;
-}
-"""
-
+with open("board-style.css", "r") as f:
+    style = f.read()
 
 
 @app.route("/")
